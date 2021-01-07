7f48dbbe4da9f912abf520508f09d981b3ccd109
Ferocious17 <caner.kaya2003@outlook.com>
2021-01-07 13:33:49 +0100

    Initial Commit

---
+++ BaseChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseChat
+//
+//  Created by Caner Kaya on 07.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
